67d19e4fad47fe886dfd8d32ccfdec1cf2d179c9
romanrts <null84-inetservice1845@yahoo.com>
2023-07-08 15:59:43 +1000

    doc(utils.mjs): add JSDoc

--- js/utils.mjs
+++ js/utils.mjs
@@ -1,5 +1,8 @@
-import data from './data.mjs'
-
+/**
+ * Replaces invalid characters with valid mnemonics and returns a string
+ * @param string
+ * @returns {string}
+ */
 export const escapeString = (string) => {
     const symbols = {
         '<': '&lt;',
@@ -12,14 +15,32 @@ export const escapeString = (string) => {
     })
 }
 
+/**
+ * Searches an array and returns an object with the specified ID property value
+ * @param id {int}
+ * @param array {Object[]}
+ * @returns {Object}
+ */
 export const getDataByID = (id, array) => {
     return array.find(item => item.id === id);
 };
 
-export const dateFormat = (date) => {
-    return new Date(date).toLocaleDateString('ru-RU', {day: 'numeric', month: 'long'})
+/**
+ * Convert date string to format: March 11.
+ * @param date {string}
+ * @param locale {string} desired language locale
+ * @returns {string}
+ */
+export const dateFormat = (date, locale = 'ru-RU') => {
+    return new Date(date).toLocaleDateString(locale, {day: 'numeric', month: 'long'})
 }
 
+/**
+ * Validates input and returns "data" if it's correct, or "fallback" if it's invalid.
+ * @param data
+ * @param fallback
+ * @returns {*}
+ */
 export const getValidData = (data, fallback) => {
     return data ? data : fallback;
 }
